483ea6c892c682db47873a984345d3138e65b599
Dhaarun <dhaarunabhimanyu@gmail.com>
2025-10-18 18:05:09 +0530

    Initialised an express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello nigga"
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
